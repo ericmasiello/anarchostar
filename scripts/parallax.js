514b3896551a2f98e64d14fe6352c0199dda2434
ericmasiello <eric.j.masiello@gmail.com>
2014-05-25 22:45:14 -0400

    Refactor parallax code

--- scripts/parallax.js
+++ scripts/parallax.js
@@ -11,7 +11,7 @@ http://www.opensource.org/licenses/mit-license.php
 http://www.gnu.org/licenses/gpl.html
 */
 
-;( function(){
+;(function(){
 
   'use strict';
 
@@ -38,8 +38,8 @@ http://www.gnu.org/licenses/gpl.html
 
       this._windowHeight = $(window).height();
 
-      that.move(); //move the background images in relation to the movement of the scrollbar
       that.resizePanel();
+      that.move(); //move the background images in relation to the movement of the scrollbar
 
     }).bind( 'scroll', function(){ //when the user is scrolling...
 
@@ -47,18 +47,6 @@ http://www.gnu.org/licenses/gpl.html
 
       that.move(); //move the background images in relation to the movement of the scrollbar
     });
-  };
-
-  //function that is called for every pixel the user scrolls. Determines the position of the background
-  /*arguments:
-   x = horizontal position of background
-   windowHeight = height of the viewport
-   pos = position of the scrollbar
-   adjuster = adjust the position of the background
-   inertia = how fast the background moves in relation to scrolling
-   */
-  Parallax.prototype._newPos = function( x, windowHeight, pos, adjuster, inertia ){
-    return x + "% " + (-(( window.PARALLAX.height + pos) - adjuster) * inertia)  + "px";
   };
 
   Parallax.prototype._removeActive = function(){
@@ -72,20 +60,37 @@ http://www.gnu.org/licenses/gpl.html
     $('.parallax-container').height( panelHeight );
   };
 
+  //function that is called for every pixel the user scrolls. Determines the position of the background
+  /*arguments:
+   x = horizontal position of background
+   windowHeight = height of the viewport
+   pos = position of the scrollbar
+   adjuster = adjust the position of the background
+   inertia = how fast the background moves in relation to scrolling
+   */
+  Parallax.prototype._newPos = function( x, windowHeight, pos, adjuster, inertia ){
+
+    return x + "% " + (-(( window.PARALLAX.height + pos) - adjuster) * inertia)  + "px";
+  };
+
   Parallax.prototype.move = function(){
 
     var pos = $(window).scrollTop(); //position of the scrollbar
-    var blockAdjust = null;
     var firstBlockLink = null;
     var that = this;
 
     $('.parallax-container').each( function( i ){
 
-      var blockAdjust = 0;
-
-      $('#pixels').html( pos ); //display the number of pixels scrolled at the bottom of the page
+      //$('#pixels').html( pos ); //display the number of pixels scrolled at the bottom of the page
 
       if( $(this).hasClass("inview") === true ) {
+
+        if( $(this).attr("id") === "block5"){
+
+          console.log( $(this).css('backgroundPosition') );
+        }
+
+        //.find('.bg1') represent the extra image added, not the background image
 
         $(this).css({
           'backgroundPosition': that._newPos( 50, this._windowHeight, pos, ( window.PARALLAX.height * ( i+1 ) ), 0.3 )
@@ -110,84 +115,4 @@ http://www.gnu.org/licenses/gpl.html
     new Parallax();
   });
 
-} )();
-
-//$(document).ready(function() { //when the document is ready...
-//
-//	//save selectors as variables to increase performance
-//	var $window = $(window);
-//	var windowHeight = $window.height(); //get the height of the window
-//
-//	//apply the class "inview" to a section that is in the viewport
-//	$('.parallax-container').bind('inview', function (event, visible) {
-//			if (visible == true) {
-//			$(this).addClass("inview");
-//			} else {
-//			$(this).removeClass("inview");
-//			}
-//		});
-//
-//	function removeActive() {
-//
-//			$("a.block-link.active").removeClass("active");
-//	}
-//
-//	//function that is called for every pixel the user scrolls. Determines the position of the background
-//	/*arguments:
-//		x = horizontal position of background
-//		windowHeight = height of the viewport
-//		pos = position of the scrollbar
-//		adjuster = adjust the position of the background
-//		inertia = how fast the background moves in relation to scrolling
-//	*/
-//	function newPos(x, windowHeight, pos, adjuster, inertia){
-//		return x + "% " + (-(( window.PARALLAX.height + pos) - adjuster) * inertia)  + "px";
-//	}
-//
-//	//function to be called whenever the window is scrolled or resized
-//
-//	function Move(){
-//		var pos = $window.scrollTop(); //position of the scrollbar
-//		var blockAdjust = null;
-//		$('.parallax-container').each(function(i) {
-//	    	var blockAdjust=0;
-//	    	$('#pixels').html(pos); //display the number of pixels scrolled at the bottom of the page
-//	    	if($(this).hasClass("inview"))
-//	    	{
-//	        	$(this).css({'backgroundPosition': newPos(50, windowHeight, pos, (window.PARALLAX.height * (i+1)), 0.3)});
-//	        	$(this).find('.bg1').css({'backgroundPosition': newPos(50, windowHeight, pos, (window.PARALLAX.height * (i+1))+window.PARALLAX.trainerBump, 0.6)});
-//	    	}
-//		});
-//		var scrollPosition = $("body").scrollTop();
-//		var firstBlockId = $('.inview').first().attr('id');
-//
-//
-//		firstBlockLink = firstBlockId.replace("block","li#blockLink") + ' a' ;
-//		removeActive();
-//		$(firstBlockLink).addClass("active");
-//	};
-//
-//  /*
-//   * Responsible for resizing the panels on resize of the panel
-//   */
-//  function ResizePanel(){
-//
-//    var headerHeight = $("#header").height();
-//    var windowHeight = $window.height();
-//    var panelHeight = ( ( windowHeight - headerHeight ) > window.PARALLAX.height ) ? ( windowHeight - headerHeight ) : window.PARALLAX.height;
-//    $(".post").height( panelHeight );
-//
-//  };
-//
-//	ResizePanel();
-//
-//	$window.resize(function(){ //if the user resizes the window...
-//		Move(); //move the background images in relation to the movement of the scrollbar
-//    ResizePanel();
-//	});
-//
-//	$window.bind('scroll', function(){ //when the user is scrolling...
-//		Move(); //move the background images in relation to the movement of the scrollbar
-//	});
-//
-//});
+})();
