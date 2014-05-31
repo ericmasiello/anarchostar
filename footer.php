d83dbd40fa6e95c710e9faad0411a9e333f8522d
ericmasiello <eric.j.masiello@gmail.com>
2014-05-31 14:43:42 -0400

    Adding back the footer content for now

--- footer.php
+++ footer.php
@@ -1,4 +1,28 @@
 	<div id="footer" class="clearfix">
+		<div class="main clearfix">
+
+		<?php
+			if(is_front_page() && is_active_sidebar('footer_home')) : dynamic_sidebar('footer_home');
+		else : ?>
+
+			<?php if (!dynamic_sidebar('footer_default') && is_user_logged_in()) : ?>
+
+			<div class = "miniFeature32 oneThird widget_tia_mini_feature footerBox">
+				<?php include( TEMPLATEPATH . '/includes/no_widgets.php'); ?>
+			</div><!-- end footer box -->
+
+			<?php endif; ?>
+
+		<?php endif; ?>
+		</div><!-- end footer main -->
+
+		<div class="secondary clearfix">
+         <?php include( TEMPLATEPATH . '/includes/socialNetworks.php'); ?>
+			<?php $footer_left = tia_get_option('tia_footer_left'); ?>
+			<?php $footer_right = tia_get_option('tia_footer_right'); ?>
+			<p class="left"><?php if($footer_left){echo($footer_left);} else{ ?>&copy; <?php echo date('Y');?> <a href="<?php bloginfo('url'); ?>"><strong><?php bloginfo('name'); ?></strong></a> All Rights Reserved.<?php }; ?></p>
+			<p class="right"><?php if($footer_right){echo($footer_right);} else{ ?><a href="http://themespectrum.com" title="Theme Spectrum">Premium Wordpress Theme by Theme Spectrum</a><?php }; ?></p>
+		</div><!-- end footer secondary-->
 
 	</div><!-- end footer -->	
 </div>
