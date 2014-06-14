8b793a795e04ab1dc1b5b66acdbd564cb1ff70f9
ericmasiello <eric.j.masiello@gmail.com>
2014-06-14 15:22:12 -0400

    Fix padding for medium+ screen sizes comment out unused options in admin panel

--- admin/options.php
+++ admin/options.php
@@ -68,7 +68,8 @@ global $tia_theme_name, $tia_theme_version, $tia_default_slideshow_speed, $tia_o
 				</div>
 				
 				<div id="option1" class="optionContent">
-					<!-- Logo -->					
+					<!-- Logo -->
+                    <!--
 					<div class="adminModule">
 						<h3 class="logoTitle">Logo</h3>	    			
 				    	
@@ -86,7 +87,8 @@ global $tia_theme_name, $tia_theme_version, $tia_default_slideshow_speed, $tia_o
 						<p class="instructions">Enter a URL of your custom logo. You can use the <a href="<?php bloginfo('url'); ?>/wp-admin/media-new.php">media uploader</a> to get your image. Make sure you copy the URL of the logo before returning here.</p>					
 		 										
 					</div>
-                      				<!-- May incorporate this later			
+					-->
+               <!-- May incorporate this later
 				<div class="adminModule clearfix">
 					<h3 class="settingsTitle">Padding Adjustment</h3>					
 					
@@ -116,8 +118,9 @@ global $tia_theme_name, $tia_theme_version, $tia_default_slideshow_speed, $tia_o
                     </div>
 
                     <!-- Background -->
+                    <!--
                     <div class="adminModule">
-                    <h3 class="settingsTitle">Background</h3>
+                        <h3 class="settingsTitle">Background</h3>
                     	<div class="smallBottomMargin clearfix">		
                             <label class="singleLine">Background:</label> 
                             <select name="tia_options[tia_theme_bkg]" id="themeBkg" class="tiaSelect inlineItem">
@@ -126,15 +129,19 @@ global $tia_theme_name, $tia_theme_version, $tia_default_slideshow_speed, $tia_o
                             </select>
 						</div>						
                     </div>
+                    -->
                     
 					<!-- CSS -->
+					<!--
 					<div class="adminModule">
 						<h3 class="settingsTitle">Custom CSS</h3>
 						<textarea name="tia_options[tia_custom_css]" cols=70 rows=6><?php echo $tia_options['tia_custom_css']; ?></textarea>
 						<p class="instructions">Enter custom CSS here. </p>
 					</div>
+					-->
 					
 					<!-- Footer Text -->
+					<!--
 					<div class="adminModule">
 						<h3 class="editTitle">Footer Text</h3>
 						
@@ -145,7 +152,8 @@ global $tia_theme_name, $tia_theme_version, $tia_default_slideshow_speed, $tia_o
 						<h4>Right side:</h4>
 						<textarea name="tia_options[tia_footer_right]" cols=70 rows=6><?php echo $tia_options['tia_footer_right']; ?></textarea>
 						<p class="instructions">This will appear on the right side of the footer.</p>
-					</div>					
+					</div>
+					-->
 					
 				</div>		
 			
