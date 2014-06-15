6c7087cb85b9ca3d03caa78da9ad165181bfaf95
ericmasiello <eric.j.masiello@gmail.com>
2014-06-15 12:30:29 -0400

    Form styling

--- page-contact.php
+++ page-contact.php
@@ -24,7 +24,7 @@
 
                 <div class="post">
                     <?php if(!is_front_page()):?>
-                        <h1><?php the_title(); ?></h1>
+                        <h1 class="giga"><?php the_title(); ?></h1>
                     <?php endif; ?>
                         <?php while (have_posts()) : the_post(); ?>
                         <?php the_content(); ?>
