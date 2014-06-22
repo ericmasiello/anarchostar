1516d8f53c3a810c00fdbc9f445fc1a96e6621b3
ericmasiello <eric.j.masiello@gmail.com>
2014-06-22 19:45:34 -0400

    Remove unneeded blog options from theme Add new social media options Add svg versions of social media icons Create custom page for contact to support social media hooks

--- includes/socialNetworks.php
+++ includes/socialNetworks.php
@@ -1,57 +1,80 @@
-<div id="socialNetworks">
-	
-	<?php if(tia_get_option('tia_facebook')) : ?>
-            <a href="<?php echo tia_get_option('tia_facebook'); ?>" title="on facebook"><img src="<?php bloginfo('template_url'); ?>/images/icons/socialmedia/facebook-20.png" alt="facebook" /></a>
+<ul class="social-media">
+    <?php if(tia_get_option('tia_facebook')) : ?>
+        <li><a href="<?php echo tia_get_option('tia_facebook'); ?>" title="on facebook"><img src="<?php bloginfo('template_url'); ?>/images/icons/socialmediasvg/webicon-facebook.svg" alt="facebook" /></a></li>
     <?php endif; ?>
     <?php if(tia_get_option('tia_twitter')) : ?>
-            <a href="<?php echo tia_get_option('tia_twitter'); ?>" title="on twitter"><img src="<?php bloginfo('template_url'); ?>/images/icons/socialmedia/twitter-20.png" alt="facebook" /></a>
+        <li><a href="<?php echo tia_get_option('tia_twitter'); ?>" title="on twitter"><img src="<?php bloginfo('template_url'); ?>/images/icons/socialmediasvg/webicon-twitter.svg" alt="facebook" /></a></li>
     <?php endif; ?>
     <?php if(tia_get_option('tia_pinterest')) : ?>
-            <a href="<?php echo tia_get_option('tia_pinterest'); ?>" title="on Pinterest" id="pinterest"><img src="<?php bloginfo('template_url'); ?>/images/icons/socialmedia/pinterest-20.png" alt="pinterest" /></a>
+        <li><a href="<?php echo tia_get_option('tia_pinterest'); ?>" title="on Pinterest" id="pinterest"><img src="<?php bloginfo('template_url'); ?>/images/icons/socialmediasvg/webicon-pinterest.svg" alt="pinterest" /></a></li>
     <?php endif; ?>
     <?php if(tia_get_option('tia_flickr')) : ?>
-            <a href="<?php echo tia_get_option('tia_flickr'); ?>" title="on flickr"><img src="<?php bloginfo('template_url'); ?>/images/icons/socialmedia/flickr-20.png" alt="flickr" /></a>
+        <li><a href="<?php echo tia_get_option('tia_flickr'); ?>" title="on flickr"><img src="<?php bloginfo('template_url'); ?>/images/icons/socialmediasvg/webicon-flickr.svg" alt="flickr" /></a></li>
     <?php endif; ?>
     <?php if(tia_get_option('tia_tumblr')) : ?>
-            <a href="<?php echo tia_get_option('tia_tumblr'); ?>" title="on tumblr"><img src="<?php bloginfo('template_url'); ?>/images/icons/socialmedia/tumblr-20.png" alt="tumblr" /></a>
+        <li><a href="<?php echo tia_get_option('tia_tumblr'); ?>" title="on tumblr"><img src="<?php bloginfo('template_url'); ?>/images/icons/socialmediasvg/webicon-tumblr.svg" alt="tumblr" /></a></li>
     <?php endif; ?>
     <?php if(tia_get_option('tia_linkedin')) : ?>
-            <a href="<?php echo tia_get_option('tia_linkedin'); ?>" title="on linkedin"><img src="<?php bloginfo('template_url'); ?>/images/icons/socialmedia/linkedin-20.png" alt="linkedin" /></a>
+        <li><a href="<?php echo tia_get_option('tia_linkedin'); ?>" title="on linkedin"><img src="<?php bloginfo('template_url'); ?>/images/icons/socialmediasvg/webicon-linkedin.svg" alt="linkedin" /></a></li>
     <?php endif; ?>
     <?php if(tia_get_option('tia_reddit')) : ?>
-            <a href="<?php echo tia_get_option('tia_reddit'); ?>" title="on reddit"><img src="<?php bloginfo('template_url'); ?>/images/icons/socialmedia/reddit-20.png" alt="reddit" /></a>
+        <li><a href="<?php echo tia_get_option('tia_reddit'); ?>" title="on reddit"><img src="<?php bloginfo('template_url'); ?>/images/icons/socialmediasvg/webicon-reddit.svg" alt="reddit" /></a></li>
     <?php endif; ?>
     <?php if(tia_get_option('tia_delicious')) : ?>
-            <a href="<?php echo tia_get_option('tia_delicious'); ?>" title="on delicious"><img src="<?php bloginfo('template_url'); ?>/images/icons/socialmedia/delicious-20.png" alt="delicious" /></a>
+        <li><a href="<?php echo tia_get_option('tia_delicious'); ?>" title="on delicious"><img src="<?php bloginfo('template_url'); ?>/images/icons/socialmediasvg/webicon-delicious.svg" alt="delicious" /></a></li>
     <?php endif; ?>
     <?php if(tia_get_option('tia_digg')) : ?>
-            <a href="<?php echo tia_get_option('tia_digg'); ?>" title="on digg"><img src="<?php bloginfo('template_url'); ?>/images/icons/socialmedia/digg-20.png" alt="digg" /></a>
+        <li><a href="<?php echo tia_get_option('tia_digg'); ?>" title="on digg"><img src="<?php bloginfo('template_url'); ?>/images/icons/socialmediasvg/webicon-digg.svg" alt="digg" /></a></li>
     <?php endif; ?>
     <?php if(tia_get_option('tia_friendfeed')) : ?>
-            <a href="<?php echo tia_get_option('tia_friendfeed'); ?>" title="on friendfeed"><img src="<?php bloginfo('template_url'); ?>/images/icons/socialmedia/friendfeed-20.png" alt="friendfeed" /></a>
+        <li><a href="<?php echo tia_get_option('tia_friendfeed'); ?>" title="on friendfeed"><img src="<?php bloginfo('template_url'); ?>/images/icons/socialmediasvg/webicon-friendfeed.svg" alt="friendfeed" /></a></li>
     <?php endif; ?>
     <?php if(tia_get_option('tia_stumbleupon')) : ?>
-            <a href="<?php echo tia_get_option('tia_stumbleupon'); ?>" title="on stumbleupon"><img src="<?php bloginfo('template_url'); ?>/images/icons/socialmedia/stumbleupon-20.png" alt="stumbleupon" /></a>
+        <li><a href="<?php echo tia_get_option('tia_stumbleupon'); ?>" title="on stumbleupon"><img src="<?php bloginfo('template_url'); ?>/images/icons/socialmediasvg/webicon-stumbleupon.svg" alt="stumbleupon" /></a></li>
     <?php endif; ?>
     <?php if(tia_get_option('tia_vimeo')) : ?>
-            <a href="<?php echo tia_get_option('tia_vimeo'); ?>" title="on vimeo"><img src="<?php bloginfo('template_url'); ?>/images/icons/socialmedia/vimeo-20.png" alt="vimeo" /></a>
+        <li><a href="<?php echo tia_get_option('tia_vimeo'); ?>" title="on vimeo"><img src="<?php bloginfo('template_url'); ?>/images/icons/socialmediasvg/webicon-vimeo.svg" alt="vimeo" /></a></li>
     <?php endif; ?>
     <?php if(tia_get_option('tia_designfloat')) : ?>
-            <a href="<?php echo tia_get_option('tia_designfloat'); ?>" title="on designfloat"><img src="<?php bloginfo('template_url'); ?>/images/icons/socialmedia/designfloat-20.png" alt="designfloat" /></a>
+        <li><a href="<?php echo tia_get_option('tia_designfloat'); ?>" title="on designfloat"><img src="<?php bloginfo('template_url'); ?>/images/icons/socialmediasvg/webicon-designfloat.svg" alt="designfloat" /></a></li>
     <?php endif; ?>
     <?php if(tia_get_option('tia_blogger')) : ?>
-            <a href="<?php echo tia_get_option('tia_blogger'); ?>" title="on blogger"><img src="<?php bloginfo('template_url'); ?>/images/icons/socialmedia/blogger-20.png" alt="blogger" /></a>
+        <li><a href="<?php echo tia_get_option('tia_blogger'); ?>" title="on blogger"><img src="<?php bloginfo('template_url'); ?>/images/icons/socialmediasvg/webicon-blogger.svg" alt="blogger" /></a></li>
     <?php endif; ?>
     <?php if(tia_get_option('tia_google')) : ?>
-            <a href="<?php echo tia_get_option('tia_google'); ?>" title="on Google"><img src="<?php bloginfo('template_url'); ?>/images/icons/socialmedia/google-20.png" alt="Google" /></a>
+        <li><a href="<?php echo tia_get_option('tia_google'); ?>" title="on Google Plus"><img src="<?php bloginfo('template_url'); ?>/images/icons/socialmediasvg/webicon-googleplus.svg" alt="Google Plus" /></a></li>
     <?php endif; ?>
     <?php if(tia_get_option('tia_youtube')) : ?>
-            <a href="<?php echo tia_get_option('tia_youtube'); ?>" title="on youtube"><img src="<?php bloginfo('template_url'); ?>/images/icons/socialmedia/youtube-20.png" alt="youtube" /></a>
+        <li><a href="<?php echo tia_get_option('tia_youtube'); ?>" title="on youtube"><img src="<?php bloginfo('template_url'); ?>/images/icons/socialmediasvg/webicon-youtube.svg" alt="youtube" /></a></li>
     <?php endif; ?>
+
+    <?php if(tia_get_option('tia_instagram')) : ?>
+        <li><a href="<?php echo tia_get_option('tia_instagram'); ?>" title="on instagram"><img src="<?php bloginfo('template_url'); ?>/images/icons/socialmediasvg/webicon-instagram.svg" alt="instagram" /></a></li>
+    <?php endif; ?>
+
+    <?php if(tia_get_option('tia_lastfm')) : ?>
+        <li><a href="<?php echo tia_get_option('tia_lastfm'); ?>" title="on last.fm"><img src="<?php bloginfo('template_url'); ?>/images/icons/socialmediasvg/webicon-lastfm.svg" alt="last.fm" /></a></li>
+    <?php endif; ?>
+
+    <?php if(tia_get_option('tia_skype')) : ?>
+        <li><a href="<?php echo tia_get_option('tia_skype'); ?>" title="on skype"><img src="<?php bloginfo('template_url'); ?>/images/icons/socialmediasvg/webicon-skype.svg" alt="skype" /></a></li>
+    <?php endif; ?>
+
+    <?php if(tia_get_option('tia_soundcloud')) : ?>
+        <li><a href="<?php echo tia_get_option('tia_soundcloud'); ?>" title="on soundcloud"><img src="<?php bloginfo('template_url'); ?>/images/icons/socialmediasvg/webicon-soundcloud.svg" alt="soundcloud" /></a></li>
+    <?php endif; ?>
+
+    <?php if(tia_get_option('tia_vine')) : ?>
+        <li><a href="<?php echo tia_get_option('tia_vine'); ?>" title="on vine"><img src="<?php bloginfo('template_url'); ?>/images/icons/socialmediasvg/webicon-vine.svg" alt="vine" /></a></li>
+    <?php endif; ?>
+
+
+	<!--
 	<?php $tia_rss = tia_get_option('tia_rss'); ?>
     <?php if($tia_rss) : ?>
-        <a href="<?php echo $tia_rss; ?>" class="subscribe"><img src="<?php bloginfo('template_url'); ?>/images/icons/socialmedia/rss-20.png" alt="rss feed" /></a>
+        <li><a href="<?php echo $tia_rss; ?>" class="subscribe"><img src="<?php bloginfo('template_url'); ?>/images/icons/socialmediasvg/webicon-rss.svg" alt="rss feed" /></a></li>
     <?php else : ?>
-        <a href="<?php echo get_bloginfo('rss2_url') ?>" class="subscribe"><img src="<?php bloginfo('template_url'); ?>/images/icons/socialmedia/rss-20.png" alt="rss feed" /></a>
+        <li><a href="<?php echo get_bloginfo('rss2_url') ?>" class="subscribe"><img src="<?php bloginfo('template_url'); ?>/images/icons/socialmediasvg/webicon-rss.svg" alt="rss feed" /></a></li>
     <?php  endif; ?>
-</div>
+    -->
+</ul>
