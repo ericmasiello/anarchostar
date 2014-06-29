1ac4c9f1fef5ac3d5f74e595541232c3c87828b2
ericmasiello <eric.j.masiello@gmail.com>
2014-06-29 10:26:20 -0400

    Mobile nav

--- scripts/mobile-nav.js
+++ scripts/mobile-nav.js
@@ -15,6 +15,11 @@
 
   'use strict';
 
+  var MAX_SIZE_S = 225;
+  var MAX_SIZE_M = 300;
+  var MQ_M = 768;
+  var SPACER_SIZE = 10;
+
   var MobileNav = function(){
 
     this._$menuIcon = $(".js-menu-icon");
@@ -26,19 +31,25 @@
   MobileNav.prototype._bindEvents = function(){
 
     this._$menuIcon.on("click", this._toggleNav.bind( this ) );
+    $(document).keyup(this._toggleNav.bind( this ));
   };
 
-  MobileNav.prototype._toggleNav = function(){
+  MobileNav.prototype._toggleNav = function( e ){
 
-    if( this._$mobileNav.is(":visible") === true ){
+    if( this._$mobileNav.is(":visible") === true
+      && ( e.type === "click" || e.type === "keyup" && e.keyCode === 27 ) ){
 
       this._$mobileNav.hide();
 
-    } else {
+    } else if( e.type === "click" ) {
 
+      var windowWidth = $(document).width();
       var menuLength = this._$mobileNav.find(".menu-item").length;
-      var size = (this._$mobileNav.show().height())/menuLength;
-      this._$mobileNav.find(".menu-item").css({"height": size + 'px', "width": size + 'px'})
+      var size = ( windowWidth >= MQ_M ) ? (this._$mobileNav.width())/menuLength - ( SPACER_SIZE * (menuLength-1) ) : (this._$mobileNav.height())/menuLength  - ( SPACER_SIZE * (menuLength-1) );
+
+      size = ( windowWidth >= MQ_M && size > MAX_SIZE_M ) ? MAX_SIZE_M : ( windowWidth < MQ_M && size > MAX_SIZE_S ? MAX_SIZE_S : size )
+      //size = ( size > MAX_SIZE_S ) ? MAX_SIZE_S : size;
+      this._$mobileNav.show().find(".menu-item").css({"height": size + 'px', "width": size + 'px'})
     }
 
   };
