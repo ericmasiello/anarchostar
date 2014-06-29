87a7913cc0d187af4d59382c94415403ea03c897
ericmasiello <eric.j.masiello@gmail.com>
2014-06-29 19:39:31 -0400

    Fix iPhone landscape mobile menu Turn hamburger into x when nav is open

--- scripts/mobile-nav.js
+++ scripts/mobile-nav.js
@@ -17,8 +17,9 @@
 
   var MAX_SIZE_S = 225;
   var MAX_SIZE_M = 300;
-  var MQ_M = 768;
+  var MQ_S = 500;
   var SPACER_SIZE = 10;
+  var CSS_OPEN_CLASS = "is-open";
 
   var MobileNav = function(){
 
@@ -36,22 +37,27 @@
 
   MobileNav.prototype._toggleNav = function( e ){
 
-    if( this._$mobileNav.is(":visible") === true
+    if( this._$menuIcon.hasClass( CSS_OPEN_CLASS ) === true
       && ( e.type === "click" || e.type === "keyup" && e.keyCode === 27 ) ){
 
       this._$mobileNav.hide();
+
+      this._$menuIcon.toggleClass( CSS_OPEN_CLASS );
+      e.preventDefault()
 
     } else if( e.type === "click" ) {
 
       var windowWidth = $(document).width();
       var menuLength = this._$mobileNav.find(".menu-item").length;
-      var size = ( windowWidth >= MQ_M ) ? (this._$mobileNav.width())/menuLength - ( SPACER_SIZE * (menuLength-1) ) : (this._$mobileNav.height())/menuLength  - ( SPACER_SIZE * (menuLength-1) );
+      var size = ( windowWidth >= MQ_S ) ? (this._$mobileNav.width())/menuLength - ( SPACER_SIZE * (menuLength-1) ) : (this._$mobileNav.height())/menuLength  - ( SPACER_SIZE * (menuLength-1) );
 
-      size = ( windowWidth >= MQ_M && size > MAX_SIZE_M ) ? MAX_SIZE_M : ( windowWidth < MQ_M && size > MAX_SIZE_S ? MAX_SIZE_S : size )
+      size = ( windowWidth >= MQ_S && size > MAX_SIZE_M ) ? MAX_SIZE_M : ( windowWidth < MQ_S && size > MAX_SIZE_S ? MAX_SIZE_S : size )
       //size = ( size > MAX_SIZE_S ) ? MAX_SIZE_S : size;
       this._$mobileNav.show().find(".menu-item").css({"height": size + 'px', "width": size + 'px'})
-    }
 
+      this._$menuIcon.toggleClass( CSS_OPEN_CLASS );
+      e.preventDefault()
+    }
   };
 
   $(document).ready(function(){
