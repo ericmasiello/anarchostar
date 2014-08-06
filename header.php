0f67b44d54ae7a8240ad69d5af00ed2981f9b6d5
masielle <masielle@advisory.com>
2014-08-06 17:06:59 -0400

    Add favicons

--- header.php
+++ header.php
@@ -12,6 +12,27 @@
 	<link rel="alternate" type="application/rss+xml" title="<?php bloginfo('name'); ?> RSS Feed" href="<?php bloginfo('rss2_url'); ?>" />
 	<link rel="alternate" type="application/atom+xml" title="<?php bloginfo('name'); ?> Atom Feed" href="<?php bloginfo('atom_url'); ?>" />
 	<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
+	
+
+  <link rel="shortcut icon" href="<?php bloginfo('template_directory');?>/favicon.ico">
+  <link rel="apple-touch-icon" sizes="57x57" href="<?php bloginfo('template_directory');?>/apple-touch-icon-57x57.png">
+  <link rel="apple-touch-icon" sizes="114x114" href="<?php bloginfo('template_directory');?>/apple-touch-icon-114x114.png">
+  <link rel="apple-touch-icon" sizes="72x72" href="<?php bloginfo('template_directory');?>/apple-touch-icon-72x72.png">
+  <link rel="apple-touch-icon" sizes="144x144" href="<?php bloginfo('template_directory');?>/apple-touch-icon-144x144.png">
+  <link rel="apple-touch-icon" sizes="60x60" href="<?php bloginfo('template_directory');?>/apple-touch-icon-60x60.png">
+  <link rel="apple-touch-icon" sizes="120x120" href="<?php bloginfo('template_directory');?>/apple-touch-icon-120x120.png">
+  <link rel="apple-touch-icon" sizes="76x76" href="<?php bloginfo('template_directory');?>/apple-touch-icon-76x76.png">
+  <link rel="apple-touch-icon" sizes="152x152" href="<?php bloginfo('template_directory');?>/apple-touch-icon-152x152.png">
+  <link rel="icon" type="image/png" href="<?php bloginfo('template_directory');?>/favicon-196x196.png" sizes="196x196">
+  <link rel="icon" type="image/png" href="<?php bloginfo('template_directory');?>/favicon-160x160.png" sizes="160x160">
+  <link rel="icon" type="image/png" href="<?php bloginfo('template_directory');?>/favicon-96x96.png" sizes="96x96">
+  <link rel="icon" type="image/png" href="<?php bloginfo('template_directory');?>/favicon-16x16.png" sizes="16x16">
+  <link rel="icon" type="image/png" href="<?php bloginfo('template_directory');?>/favicon-32x32.png" sizes="32x32">
+  <meta name="msapplication-TileColor" content="#b91d47">
+  <meta name="msapplication-TileImage" content="<?php bloginfo('template_directory');?>/mstile-144x144.png">
+  <meta name="msapplication-config" content="<?php bloginfo('template_directory');?>/browserconfig.xml">
+
+
 	<?php $heading_font = tia_get_option('tia_header_font'); ?>
 	<?php $body_font = tia_get_option('tia_body_font'); ?>
 	<?php if ($heading_font != "") { ?>
